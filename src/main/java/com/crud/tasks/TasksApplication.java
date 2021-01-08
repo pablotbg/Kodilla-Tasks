f47ca7ae0af689d12b9425c9a461753f83206f65
Paweł Praga <p.praga@o2.pl>
2021-01-08 11:47:55 +0100

    18.4 Spring WEB

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,6 +1,5 @@
 package com.crud.tasks;
 
-import com.crud.tasks.domain.TaskDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class TaskDTO {
+public class TaskDto {
     private Long id;
     private String title;
     private String content;
